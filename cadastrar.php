be7fde5020d98be56c80d74605800d8277902530
camile <85092924004@senacrs.edu.br>
2025-07-02 09:13:11 -0300

    commit

--- cadastrar.php
+++ cadastrar.php
@@ -7,6 +7,7 @@ include_once './includes/header-login.php';
     <div id="paginalogin">
 
     <div id="carddelogin">
+        <form action="./processa-login.php" method="post">
         <div id="titulo">
             <h1>Cadastre-se</h1>
         </div>
@@ -15,31 +16,32 @@ include_once './includes/header-login.php';
         </div>
         <div id="informacoes">
             <p class="texto">E-MAIL:</p>
-            <input type="text">
+            <input type="text" class="preencher">
             <div class="linha"></div>
             <p class="texto">CPF:</p>
-            <input type="text">
+            <input type="text" class="preencher">
             <div class="linha"></div>
             <p class="texto">NOME COMPLETO:</p>
-            <input type="text">
+            <input type="text" class="preencher">
             <div class="linha"></div>
             <p class="texto">DATA DE NASCIMENTO:</p>
-            <input type="text">
+            <input type="text" class="preencher">
             <div class="linha"></div>
             <p class="texto">NOME DE USUÁRIO:</p>
-            <input type="text">
+            <input type="text" class="preencher">
             <div class="linha"></div>
             <p class="texto">SENHA:</p>
-            <input type="text">
+            <input type="text" class="preencher">
             <div class="linha"></div>
-            <button class="botao" id="entrar">CADASTRAR</button>
-            <div id="linhascomou">
-            <div class="linhapequena"></div>
-            <p id="ou">OU</p>
-            <div class="linhapequena"></div>
+                <input type="submit" class="botao" id="entrar" value="CADASTRAR"></input>
+                <div id="linhascomou">
+                <div class="linhapequena"></div>
+                <p id="ou">OU</p>
+                <div class="linhapequena"></div>
+                </div>
+                <a class="botao" id="querocadastrar" href="./login.php">QUERO FAZER LOGIN</a>
             </div>
-            <button class="botao" id="querocadastrar">QUERO FAZER LOGIN</button>
-        </div>
+        </form>
     </div>
     </div>
 </body>
--- login.php
+++ login.php
@@ -7,24 +7,26 @@ include_once './includes/header-login.php';
     <div id="paginalogin">
 
     <div id="carddelogin">
-        <div id="titulo">
-            <h1>Faça seu login</h1>
-        </div>
-        <div id="informacoes">
-            <p class="texto">E-MAIL:</p>
-            <input type="text">
-            <div class="linha"></div>
-            <p class="texto">SENHA:</p>
-            <input type="text">
-            <div class="linha"></div>
-            <button class="botao" id="entrar">ENTRAR</button>
-            <div id="linhascomou">
-            <div class="linhapequena"></div>
-            <p id="ou">OU</p>
-            <div class="linhapequena"></div>
+        <form action="./processa-login.php" method="post">
+            <div id="titulo">
+                <h1>Faça seu login</h1>
             </div>
-            <button class="botao" id="querocadastrar" href="./cadastrar.php">QUERO ME CADASTRAR</button>
-        </div>
+            <div id="informacoes">
+                <p class="texto">E-MAIL:</p>
+                <input type="text" class="preencher">
+                <div class="linha"></div>
+                <p class="texto">SENHA:</p>
+                <input type="text" class="preencher">
+                <div class="linha"></div>
+                <input type="submit" class="botao" id="entrar" value="ENTRAR"></input>
+                <div id="linhascomou">
+                <div class="linhapequena"></div>
+                <p id="ou">OU</p>
+                <div class="linhapequena"></div>
+                </div>
+                <a class="botao" id="querocadastrar" href="./cadastrar.php">QUERO ME CADASTRAR</a>
+            </div>
+        </form>
     </div>
     </div>
 </body>
