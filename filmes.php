135abc0baa75f8cbd9d9be55f5a3cdfd19344baa
rafaela <rafaeladf2007@gmail.com>
2025-07-09 10:06:11 -0300

    pagina com layout pronto

--- filmes.php
+++ filmes.php
@@ -1,49 +1,225 @@
-<?php
-include_once './includes/conexao.php';
-$pagina = 'filmes';
-include_once './includes/header-login.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/filmes.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"><style>
+      @import url('https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400..900&family=Questrial&family=Shrikhand&display=swap');
+      @import url('https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400..900&family=Questrial&display=swap');
+      @import url('https://fonts.googleapis.com/css2?family=Bree+Serif&family=Maven+Pro:wght@400..900&family=Questrial&family=Shrikhand&display=swap');
+    </style>
+    <title>Filmes Recomendados</title>
+</head>
 <body>
-    <main>
-        <div id="pagina6">
-            <h1 id="titulo">Filmes recomendados</h1>
-            <div class="caixa">
+  <header>
+    <div id="first">
+      <h1>Filmes <br> Recomendados</h1>
+    </div>
+  </header>
+  <main>
+    <div class="accordion">
 
-                <div class="caixatext">
-                  <div class="parteimg">
-                    <img class="poster" src="imgs/posterclube da luta.jpg" alt="">
+          <div class="accordion-content">
+              <div class="header">
+                  <img class="capas" src="imgs/img/clcapa.png" alt="">
+                  <div class="textheader">
+                      <span class="title">Clube da Luta</span>
+                      <p class="sinop">Um homem deprimido que sofre de insônia conhece um estranho vendedor chamado Tyler Durden e se vê morando em uma casa suja depois que seu perfeito apartamento é destruído. A dupla forma um clube com regras rígidas onde homens lutam.</p>
                   </div>
 
-                  <div class="partetex">
-                    <h2 class="titulocaixa">Clube da Luta</h2>
-                    <p class="textocaixa">  Um homem deprimido que sofre de insônia conhece um estranho vendedor chamado Tyler Durden e se vê morando em uma casa 
-                    suja depois que seu perfeito apartamento é destruído. A dupla forma um clube com regras rígidas onde homens lutam.</p>
+                  <div class="flecha">
+                      <i class="fa-solid fa-plus"></i>
+                  </div>
+              </div>
+              <div class="descricao">
+                  <p class="description1"> <span class="firstp">O </span> filme "Clube da Luta" de 1999 acompanha a história de um homem monótono, sem relacionamentos 
+                      ou amizades, cujo cotidiano muda após conhecer um homem em um avião. Eles criam um clube de luta, ele se envolve 
+                      com uma mulher, perde o emprego, passa a morar com o amigo e adquire novos vínculos. A narrativa destaca a mudança 
+                      na paleta de cores do filme que reflete o estado emocional do protagonista: inicialmente tons azuis e verdes que 
+                      transmitem calma, estabilidade e tristeza, representam sua rotina monótona. A partir do encontro com o outro homem, 
+                      aparecem cores vermelhas, simbolizando desejo, energia, perigo e violência, que se intensificam conforme ele se 
+                      envolve mais com esse personagem</p>
+                  <p class="description"><span class="firstp">Com </span> o tempo, a paleta evolui para tons quentes como laranja e amarelo, indicando sua 
+                      transformação emocional e perda de racionalidade, especialmente após uma cena decisiva em que ele escolhe uma porta 
+                      com luz amarelada, simbolizando sua entrada em um estado impulsivo. Ao final, uma cena marcada pelo retorno ao azul 
+                      traz à tona sentimentos de tristeza e racionalidade, concluindo a narrativa sobre as mudanças emocionais do 
+                      protagonista e a influência das cores para compreender sua trajetória.</p>
+                  <img class="primeiraft" src="imgs/img/clfoto1.png" alt="">
+                  <img src="imgs/img/clfoto2.png" alt="">
+                  <img src="imgs/img/clfoto3.png" alt="">
+
+                  <div class="comentario">
+                      <h2>Você percebeu mais alguma coisa no filme?</h2>
+                      <input id="placeholder-text" type="text" placeholder="Escreva um comentário">
+                      <a href="filmes.php" class="myButton">enviar</a>
+
+                  </div>
+              </div>
+          </div>
+
+          <div class="accordion-content">
+              <div class="header">
+                  <img class="capas" src="imgs/img/mkcapa.png" alt="">
+                  <div class="textheader">
+                      <span class="title">Moonrise Kingdom</span>
+                      <p class="sinop">Em 1965, residentes de uma ilha formam uma comunidade aparentemente imune aos males do resto do 
+                          mundo.Os adolescentes Sam e Suzy se apaixonam e decidem fugir antes da chegada de uma violenta tempestade que 
+                          ameaça a região.</p>
                   </div>
 
-                  <div class="partefle">
-                    <img class="flecha" src="imgs/flecha.png" alt="">
+                  <div class="flecha">
+                      <i class="fa-solid fa-plus"></i>
                   </div>
-            
-            </div>
-        </div>
+              </div>
+              <div class="descricao">
+                  <p class="description1"> <span class="firstp">Moonrise </span> Kingdom é um filme de 2012 dirigido por Wes Anderson, 
+                      que utiliza uma paleta de cores predominante amarela para transmitir sentimentos de nostalgia, alegria e otimismo, 
+                      além de refletir o cenário de 1965. As cores também representam as emoções dos personagens: amarelo para a juventude
+                      e imaturidade de Sam, e vermelho para a paixão e maturidade de Suzy. Quando os protagonistas se apaixonam, 
+                      predominam cenas em rosa, simbolizando inocência e romance. Durante sua fuga, o uso do azul e cinza indica tristeza,
+                      e o mundo adulto, enquanto momentos de tensão e mistério são ilustrados pelo uso de cores acinzentadas e 
+                      escuras, como preto, que representam medo e insegurança. </p>
+                  <p class="description"><span class="firstp">Quando </span> as crianças são descobertas e precisam fugir novamente, a
+                      paleta volta ao azul, expressando tristeza, e posteriormente às cores acinzentadas, simbolizando medo e mistério, 
+                      até o desfecho feliz, onde tudo volta ao amarelo, indicando satisfação e normalidade. A personagem Suzy, ao final,
+                        muda do rosa e vermelho para o amarelo, simbolizando ousadia e exuberância.</p>
+                  <img class="primeiraft" src="imgs/img/mkfoto1.png" alt="">
+                  <img src="imgs/img/mkfoto2.png" alt="">
+                  <img src="imgs/img/mkfoto3.png" alt="">
+
+                  <div class="comentario">
+                      <h2>Você percebeu mais alguma coisa no filme?</h2>
+                      <input id="placeholder-text" type="text" placeholder="Escreva um comentário">
+                      <a href="filmes.php" class="myButton">enviar</a>
+
+                  </div>
+              </div>
+          </div>
+
+          <div class="accordion-content">
+              <div class="header">
+                  <img class="capas" src="imgs/img/lllcapa.png" alt="">
+                  <div class="textheader">
+                      <span class="title">La La Land</span>
+                      <p class="sinop">O pianista Sebastian conhece a atriz Mia, e os dois se apaixonam perdidamente. Em busca de 
+                          oportunidades para suas carreiras na competitiva Los Angeles, os jovens tentam fazer o relacionamento amoroso 
+                          dar certo, enquanto perseguem fama e sucesso.</p>
+                  </div>
+
+                  <div class="flecha">
+                      <i class="fa-solid fa-plus"></i>
+                  </div>
+              </div>
+              <div class="descricao">
+                  <p class="description1"> <span class="firstp">O </span> filme "Clube da Luta" de 1999 acompanha a história de um homem monótono, sem relacionamentos 
+                      ou amizades, cujo cotidiano muda após conhecer um homem em um avião. Eles criam um clube de luta, ele se envolve 
+                      com uma mulher, perde o emprego, passa a morar com o amigo e adquire novos vínculos. A narrativa destaca a mudança 
+                      na paleta de cores do filme que reflete o estado emocional do protagonista: inicialmente tons azuis e verdes que 
+                      transmitem calma, estabilidade e tristeza, representam sua rotina monótona. A partir do encontro com o outro homem, 
+                      aparecem cores vermelhas, simbolizando desejo, energia, perigo e violência, que se intensificam conforme ele se 
+                      envolve mais com esse personagem</p>
+                  <p class="description"><span class="firstp">Com </span> o tempo, a paleta evolui para tons quentes como laranja e amarelo, indicando sua 
+                      transformação emocional e perda de racionalidade, especialmente após uma cena decisiva em que ele escolhe uma porta 
+                      com luz amarelada, simbolizando sua entrada em um estado impulsivo. Ao final, uma cena marcada pelo retorno ao azul 
+                      traz à tona sentimentos de tristeza e racionalidade, concluindo a narrativa sobre as mudanças emocionais do 
+                      protagonista e a influência das cores para compreender sua trajetória.</p>
+                  <img class="primeiraft" src="imgs/img/yourfilms em pt (2).png" alt="">
+                  <img src="imgs/img/yourfilms em pt (3).png" alt="">
+                  <img src="imgs/img/Captura de tela 2025-07-07 113450.png" alt="">
+
+                  <div class="comentario">
+                      <h2>Você percebeu mais alguma coisa no filme?</h2>
+                      <input id="placeholder-text" type="text" placeholder="Escreva um comentário">
+                      <a href="filmes.php" class="myButton">enviar</a>
+
+                  </div>
+              </div>
+          </div>
+
+          <div class="accordion-content">
+              <div class="header">
+                  <img class="capas" src="imgs/img/wcapa.png" alt="">
+                  <div class="textheader">
+                      <span class="title">Whiplash</span>
+                      <p class="sinop">Andrew sonha em ser o melhor baterista de sua geração. Ele chama a atenção do impiedoso mestre do 
+                          jazz Terence Fletcher, que ultrapassa os limites e transforma seu sonho em uma obsessão, colocando em risco a 
+                          saúde física e mental do jovem músico.</p>
+                  </div>
+
+                  <div class="flecha">
+                      <i class="fa-solid fa-plus"></i>
+                  </div>
+              </div>
+              <div class="descricao">
+                  <p class="description1"> <span class="firstp">O </span> filme "Clube da Luta" de 1999 acompanha a história de um homem monótono, sem relacionamentos 
+                      ou amizades, cujo cotidiano muda após conhecer um homem em um avião. Eles criam um clube de luta, ele se envolve 
+                      com uma mulher, perde o emprego, passa a morar com o amigo e adquire novos vínculos. A narrativa destaca a mudança 
+                      na paleta de cores do filme que reflete o estado emocional do protagonista: inicialmente tons azuis e verdes que 
+                      transmitem calma, estabilidade e tristeza, representam sua rotina monótona. A partir do encontro com o outro homem, 
+                      aparecem cores vermelhas, simbolizando desejo, energia, perigo e violência, que se intensificam conforme ele se 
+                      envolve mais com esse personagem</p>
+                  <p class="description"><span class="firstp">Com </span> o tempo, a paleta evolui para tons quentes como laranja e amarelo, indicando sua 
+                      transformação emocional e perda de racionalidade, especialmente após uma cena decisiva em que ele escolhe uma porta 
+                      com luz amarelada, simbolizando sua entrada em um estado impulsivo. Ao final, uma cena marcada pelo retorno ao azul 
+                      traz à tona sentimentos de tristeza e racionalidade, concluindo a narrativa sobre as mudanças emocionais do 
+                      protagonista e a influência das cores para compreender sua trajetória.</p>
+                  <img class="primeiraft" src="imgs/img/clfoto1.png" alt="">
+                  <img src="imgs/img/clfoto2.png" alt="">
+                  <img src="imgs/img/clfoto3.png" alt="">
+
+                  <div class="comentario">
+                      <h2>Você percebeu mais alguma coisa no filme?</h2>
+                      <input id="placeholder-text" type="text" placeholder="Escreva um comentário">
+                      <a href="filmes.php" class="myButton">enviar</a>
+
+                  </div>
+              </div>
+          </div>
+
+          <div class="accordion-content">
+              <div class="header">
+                  <img class="capas" src="imgs/img/pcapa.png" alt="">
+                  <div class="textheader">
+                      <span class="title">Pearl</span>
+                      <p class="sinop">Presa em uma fazenda isolada, Pearl deve cuidar de seu pai doente sob a vigilância de sua mãe. 
+                        Desejando a vida glamouroso que ela viu nos filmes, as tentações e repressões de Pearl colidem.</p>
+                  </div>
+
+                  <div class="flecha">
+                      <i class="fa-solid fa-plus"></i>
+                  </div>
+              </div>
+              <div class="descricao">
+                  <p class="description1"> <span class="firstp">O </span> filme "Clube da Luta" de 1999 acompanha a história de um homem monótono, sem relacionamentos 
+                      ou amizades, cujo cotidiano muda após conhecer um homem em um avião. Eles criam um clube de luta, ele se envolve 
+                      com uma mulher, perde o emprego, passa a morar com o amigo e adquire novos vínculos. A narrativa destaca a mudança 
+                      na paleta de cores do filme que reflete o estado emocional do protagonista: inicialmente tons azuis e verdes que 
+                      transmitem calma, estabilidade e tristeza, representam sua rotina monótona. A partir do encontro com o outro homem, 
+                      aparecem cores vermelhas, simbolizando desejo, energia, perigo e violência, que se intensificam conforme ele se 
+                      envolve mais com esse personagem</p>
+                  <p class="description"><span class="firstp">Com </span> o tempo, a paleta evolui para tons quentes como laranja e amarelo, indicando sua 
+                      transformação emocional e perda de racionalidade, especialmente após uma cena decisiva em que ele escolhe uma porta 
+                      com luz amarelada, simbolizando sua entrada em um estado impulsivo. Ao final, uma cena marcada pelo retorno ao azul 
+                      traz à tona sentimentos de tristeza e racionalidade, concluindo a narrativa sobre as mudanças emocionais do 
+                      protagonista e a influência das cores para compreender sua trajetória.</p>
+                  <img class="primeiraft" src="imgs/img/clfoto1.png" alt="">
+                  <img src="imgs/img/clfoto2.png" alt="">
+                  <img src="imgs/img/clfoto3.png" alt="">
+
+                  <div class="comentario">
+                      <h2>Você percebeu mais alguma coisa no filme?</h2>
+                      <input id="placeholder-text" type="text" placeholder="Escreva um comentário">
+                      <a href="filmes.php" class="myButton">enviar</a>
+
+                  </div>
+              </div>
+          </div>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </main>
+    </div>
+  </main>
+  <script src="filmes.js"></script>
 </body>
-<?php 
-include_once './includes/footer.php';
-?>
+</html>
